d2957b8d444b87dcfe55a2800fa6621f382c789c
unknown <willreubena@gmail.com>
2025-01-08 21:37:08 +0000

    created window

---
+++ src/App.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+public class App {
+    public static void main(String[] args) throws Exception {
+        // window
+        int tileSize = 36;
+        int rows = 16;
+        int cols = 16;
+        int boardHeight = tileSize * rows;
+        int boardWidth = tileSize * cols;
+
+        JFrame frame = new JFrame("Space Invaders");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
