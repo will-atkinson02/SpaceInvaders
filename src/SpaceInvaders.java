2c9ab17c306c1b1083904f561f3248066bbf2016
unknown <willreubena@gmail.com>
2025-01-09 11:33:28 +0000

    alien art

--- src/SpaceInvaders.java
+++ src/SpaceInvaders.java
@@ -12,8 +12,29 @@ public class SpaceInvaders extends JPanel {
     int boardHeight = tileSize * rows;
     int boardWidth = tileSize * cols;
 
+    Image shipImg;
+    Image alienImg;
+    Image alienCyanImage;
+    Image alienMagentaImage;
+    Image alienYellowImage;
+    ArrayList<Image> alienImgArray; 
+
     SpaceInvaders() {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
         setBackground(Color.black);
+
+        //load images
+        shipImg = new ImageIcon(getClass().getResource("./ship.png")).getImage();
+        alienImg = new ImageIcon(getClass().getResource("./alien.png")).getImage();
+        alienCyanImage = new ImageIcon(getClass().getResource("./alien-cyan.png")).getImage();
+        alienMagentaImage = new ImageIcon(getClass().getResource("./alien-magenta.png")).getImage();
+        alienYellowImage = new ImageIcon(getClass().getResource("./alien-yellow.png")).getImage();
+
+        alienImgArray = new ArrayList<Image>();
+        alienImgArray.add(alienImg);
+        alienImgArray.add(alienCyanImage);
+        alienImgArray.add(alienMagentaImage);
+        alienImgArray.add(alienYellowImage);
+
     }
 }
